95d605ecfc65eb8579936ea9f8302c75b7eb43f8
kevynsenac <kevyn.omielitz@senacsp.edu.br>
2026-08-17 11:29:00 -0300

    .

--- Semestre_1/Algoritmos_e_Programacao_1/Hello.java
+++ Semestre_1/Algoritmos_e_Programacao_1/Hello.java
@@ -1,6 +1,8 @@
+// Fundamentos de Java
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I am learning Java!");
+        System.out.println("Look at this\n line break!");
     }
 }
