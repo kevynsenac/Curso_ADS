9d39458f9deceb935b66a81766e4a8a14d267417
kevynsenac <kevyn.omielitz@senacsp.edu.br>
2026-08-17 11:00:03 -0300

    .

--- Semestre_1/Algoritmos_e_Programacao_1/Hello.java
+++ Semestre_1/Algoritmos_e_Programacao_1/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        // escrever Hello World na tela
+        // Escrever Hello World na tela
         System.out.println("Hello World");
     }
 }
