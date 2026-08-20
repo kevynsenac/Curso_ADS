2857aa1a1dc733ae3b3b7ae18707b13b9a24afb0
kevynsenac <kevyn.omielitz@senacsp.edu.br>
2026-08-20 11:17:09 -0300

    .

--- Semestre_1/Banco_de_Dados/comecando.sql
+++ Semestre_1/Banco_de_Dados/comecando.sql
@@ -1,2 +1,20 @@
 -- Ainda não fizemos nenhum exercício e o outro arquivo é um template 
 -- disponibilizado pelo professor provavelmente para aulas futuras.
+
+CREATE TABLE turma ( 
+idturma INTEGER(4) NOT NULL AUTO_INCREMENT, 
+capacidade INTEGER(2) NOT NULL, 
+idProfessor INTEGER(4) NOT NULL, 
+PRIMARY KEY (idturma), 
+FOREIGN KEY (idProfessor) REFERENCES 
+professor(idProfessor), UNIQUE KEY idturma
+(idturma) 
+);
+CREATE TABLE professor ( 
+idProfessor INTEGER(4) NOT NULL AUTO_INCREMENT, 
+telefone INTEGER(10) NOT NULL, 
+nome CHAR(80) COLLATE NOT NULL DEFAULT ‘’, 
+PRIMARY KEY (idProfessor), 
+FOREIGN KEY(idTurma) REFERENCES turma(idturma), 
+UNIQUE KEY idProfessor (idProfessor) 
+); 
